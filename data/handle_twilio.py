6bd0095425d32b6f97060951ac1f6bdc8a611b8b
kodadasiofficial <isaadahanunal@gmail.com>
2025-05-15 15:24:59 +0300

    get and request audio's added

--- data/handle_twilio.py
+++ data/handle_twilio.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 account_sid = os.getenv("TWILIO_ACCOUNT_SID")
 auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+auth = (account_sid, auth_token)
 
 def handle_image_urls(form: FormData) -> list:
     media_num = int(form.get("NumMedia",0))
     image_urls = []
-    auth = (account_sid, auth_token)
     for i in range(media_num):
         media_type = form.get(f"MediaContentType{i}")
         if media_type.startswith("image/"):
@@ -21,3 +21,16 @@ def handle_image_urls(form: FormData) -> list:
             encoded_url = f"data:image/jpeg;base64,{encoded_image}"
             image_urls.append(encoded_url)
     return image_urls
+
+def handle_audio_urls(form: FormData) -> list:
+    media_num = int(form.get("NumMedia",0))
+    audio_urls = []
+    for i in range(media_num):
+        media_type = form.get(f"MediaContentType{i}")
+        if media_type.startswith("audio/"):
+            audio_url = form.get(f"MediaUrl{i}")
+            response = requests.get(audio_url, auth=auth)       
+            encoded_audio = base64.b64encode(response.content).decode('utf-8')
+            encoded_url = f"data:audio/mpeg;base64,{encoded_audio}"
+            audio_urls.append(encoded_url)
+    return audio_urls
